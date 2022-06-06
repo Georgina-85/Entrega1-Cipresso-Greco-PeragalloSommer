992224ffdbf971c7643aa796ee3c532963647d67
Georgina-85 <georginaperagallo@gmail.com>
2022-06-06 11:38:28 -0300

    punto seguro

---
+++ AppWeb/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+# Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+
+    #def __str__(self): 
+        #return self.nombre + ' ' + self.camada
+
+
+class Estudiantes(models.Model):
+    nombre = models.CharField(max_length=40)  
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=40)  
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()   
+    profesion = models.CharField(max_length=40)
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=30)
+    fechaDeEntrega = models.DateField() 
+    entregado = models.BooleanField()   
